d7b3e91eea64899a4586813c56e1fcfed609c001
Dipika1226 <dipika26cs044@satiengg.in>
2025-07-03 15:50:04 +0530

    did some modifications to UI

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -5,7 +5,7 @@ const SecondaryContainer = () => {
     const movies = useSelector((store) => store.movies)
     return (
         <div className='bg-black'>
-            <div className='sm:mt-0 md:mt-[-2%] lg:mt-[-17%] z-20 relative'>
+            <div className='sm:mt-0 md:mt-[-2%] lg:mt-[-11%] xl:mt-[-17%] z-20 relative'>
                 <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
                 <MovieList title={"Upcoming"} movies={movies.upcomingMovies} />
                 <MovieList title={"Popular"} movies={movies.popularMovies} />
